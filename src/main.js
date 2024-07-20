d1a5d511b297c1f102a0c02ea02d43eed0d1c8f3
Suprussy <suprussy73@gmail.com>
2024-07-20 21:20:48 +0900

    router 기능 / 'router', 'views' 폴더 추가
    1. vue-router 추가
    2. 'router' 폴더 추가 - 'index.js' 추가 및 내용 입력
    3. 'views' 폴더 추가 - 'AboutView.vue', 'HomeView.vue' 추가 및 내용 입력

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from '@/router'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
 import 'bootstrap/dist/js/bootstrap.js'
