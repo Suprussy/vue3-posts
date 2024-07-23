fb056cd6845c64f941aeb8861c8357ae41b77d61
Suprussy <suprussy73@gmail.com>
2024-07-23 21:08:54 +0900

    404 Not Found & 중첩 라우트 적용

---
+++ src/views/nested/NestedView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <ul class="nav nav-tabs" role="tablist">
+      <li class="nav-item" role="presentation">
+        <router-link class="nav-link" active-class="active" :to="{name: 'NestedOne', replace: true}">Nested One</router-link>
+      </li>
+      <li class="nav-item" role="presentation">
+        <router-link class="nav-link" active-class="active" :to="{name: 'NestedTwo', replace: true}">Nested Two</router-link>
+      </li>
+    </ul>
+    <hr class="my-4">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
